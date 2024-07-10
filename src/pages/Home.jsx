63547a9f2edced99d9430bd5c2a5e3573da8f05f
Whitehead549 <finbareric549@gmail.com>
2024-07-10 16:45:45 +0100

    build complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -167,12 +167,12 @@ const Home = () => {
         <LoaderSpinner />
       ) : (
         <>
-          <div className='relative h-[700px]'>
+            <div className='relative h-[400px]'>
             <video
               autoPlay
               loop
               muted
-              className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
+              className="absolute right-0 top-0 h-[400px] w-full object-cover z-[-1]"
             >
               <source src={MainVid} type="video/mp4" />
             </video>
