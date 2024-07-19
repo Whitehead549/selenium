587a6bea4d2cb61b7c9e66412ab368dac53ddc06
Whitehead549 <finbareric549@gmail.com>
2024-07-19 02:00:13 +0100

    build complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
                                                                                     
                         <div data-aos="fade-up" data-aos-delay="600" className="flex items-center space-x-2">
                             <a href="https://wa.me/+2348181287661?text=Greeting%20engr" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2">
-                                <span className="text-blue-700 underline font-bold text-lg sm:text-lg md:text-lg lg:text-lg cursor-pointer">Click here</span>
+                                <span className="text-blue-900 underline font-bold text-lg sm:text-lg md:text-lg lg:text-lg cursor-pointer">Click here</span>
                                 <img src={whatsapp} alt="Banner" className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
                             </a>
                         </div>
