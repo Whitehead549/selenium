a18126e2fd80d5f22fd2f4c5d82ebbc944827ba3
Whitehead549 <finbareric549@gmail.com>
2024-07-19 01:54:41 +0100

    build complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                         {/* text content section */}
                         <div className='text-white pt-8 font-bold'>
                         <p data-aos="fade-up" className="text-md">
-                        Chat Engr on <span className="text-green-600">WhatApp</span>
+                        Chat Engr on <span className="text-green-700">WhatApp</span>
                         </p>
 
                         <p
@@ -28,7 +28,7 @@ const Hero = () => {
                                                                                     
                         <div data-aos="fade-up" data-aos-delay="600" className="flex items-center space-x-2">
                             <a href="https://wa.me/+2348181287661?text=Greeting%20engr" target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2">
-                                <span className="text-blue-600 underline font-bold text-lg sm:text-lg md:text-lg lg:text-lg cursor-pointer">Click here</span>
+                                <span className="text-blue-700 underline font-bold text-lg sm:text-lg md:text-lg lg:text-lg cursor-pointer">Click here</span>
                                 <img src={whatsapp} alt="Banner" className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
                             </a>
                         </div>
